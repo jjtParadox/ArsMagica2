370fafab288bf40ee2176ba700fd519ab7b437cf
Sunconure11 <eyespot7b@aol.com>
2017-02-28 09:43:35 -0500

    plankWood

--- src/main/java/am2/proxy/CommonProxy.java
+++ src/main/java/am2/proxy/CommonProxy.java
@@ -439,7 +439,7 @@ public class CommonProxy implements IGuiHandler{
 		OreDictionary.registerOre("gemSunstone", new ItemStack(ItemDefs.itemOre, 1, ItemOre.META_SUNSTONE));
 		
 		OreDictionary.registerOre("logWood", new ItemStack(BlockDefs.blocks, 1, BlockWitchwoodLog()));
-		OreDictionary.registerOre("logWood", new ItemStack(BlockDefs.blocks, 1, BlockWitchwoodLog()));		
+		OreDictionary.registerOre("plankWood", new ItemStack(BlockDefs.blocks, 1, BlockWitchwoodPlanks()));		
 
 	}
 }
