989a37bdf4d16a11a9f7d40e590796ebf166192f
Camilla Ett <camilla.ett@gmail.com>
2017-05-03 17:31:03 +0900

    Light Component become stained by ColorModifier

--- src/main/java/am2/blocks/BlockMageLight.java
+++ src/main/java/am2/blocks/BlockMageLight.java
@@ -4,14 +4,19 @@ import java.util.List;
 import java.util.Random;
 
 import am2.ArsMagica2;
+import am2.items.ItemInscriptionTable;
 import am2.particles.AMParticle;
 import am2.particles.ParticleFloatUpward;
 import am2.particles.ParticleGrow;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
@@ -26,9 +31,13 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class BlockMageLight extends BlockAMSpecialRender {
+	
+	public static final PropertyEnum<EnumDyeColor> COLOR = PropertyEnum.<EnumDyeColor>create("color", EnumDyeColor.class);
+	private int color = 0xFFFFFF;
 
 	public BlockMageLight(){
 		super(Material.CIRCUITS);
+		this.setDefaultState(blockState.getBaseState().withProperty(COLOR, EnumDyeColor.WHITE));
 		//setBlockBounds(0.35f, 0.35f, 0.35f, 0.65f, 0.65f, 0.65f);
 		this.setTickRandomly(true);
 	}
@@ -36,6 +45,27 @@ public class BlockMageLight extends BlockAMSpecialRender {
 	@Override
 	public int tickRate(World par1World){
 		return 20 - 5 * ArsMagica2.config.getGFXLevel();
+	}
+	
+	private void getRGBcolor(EnumDyeColor dye) {
+		switch(dye) {
+			case BLACK: color = 0x191919;break;
+			case RED: color = 0x993333;break;
+			case GREEN: color = 0x667F33;break;
+			case BROWN: color = 0x664C33;break;
+			case BLUE: color = 0x334CB2;break;
+			case PURPLE: color = 0x7F3FB2;break;
+			case CYAN: color = 0x4C7F99;break;
+			case SILVER: color = 0x999999;break;
+			case GRAY: color = 0x4C4C4C;break;
+			case PINK: color = 0xF27FA5;break;
+			case LIME: color = 0x7FCC19;break;
+			case YELLOW: color = 0xE5E533;break;
+			case LIGHT_BLUE: color = 0x6699D8;break;
+			case MAGENTA: color = 0xB24CD8;break;
+			case ORANGE: color = 0xD87F33;break;
+			default: color = 0xFFFFFF;break;
+		}
 	}
 	
 	@Override
@@ -46,7 +76,8 @@ public class BlockMageLight extends BlockAMSpecialRender {
 			particle.setMaxAge(10 + rand.nextInt(20));
 			particle.AddParticleController(new ParticleFloatUpward(particle, 0f, -0.01f, 1, false));
 			particle.AddParticleController(new ParticleGrow(particle, -0.005f, 1, false));
-			//particle.setRGBColorI(color);
+			getRGBcolor(stateIn.getValue(COLOR));
+			particle.setRGBColorI(color);
 		}
 	}
 	
@@ -112,5 +143,20 @@ public class BlockMageLight extends BlockAMSpecialRender {
 		this.setUnlocalizedName(rl.toString());
 		GameRegistry.register(this, rl);
 		return this;
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, COLOR);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(COLOR).getMetadata();
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(COLOR, EnumDyeColor.byMetadata(meta));
 	}
 }
